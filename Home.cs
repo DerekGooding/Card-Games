404d8c3104836a159b53aa368d5f22061c84f48a
Derek Gooding <djgooding@live.com>
2025-11-11 12:15:57 -0500

    use file-scope namespaces

--- Home.cs
+++ Home.cs
@@ -8,57 +8,56 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Poker
+namespace Poker;
+
+public partial class Home : Form
 {
-    public partial class Home : Form
+    public Home()
     {
-        public Home()
-        {
-            InitializeComponent();
-        }
-        bool blackjack = false;
-        bool poker = false;
-        private void Blackjack_Click(object sender, EventArgs e)
-        {
-            poker = false;
-            blackjack = true;
-        }
-        public List<string> listnames = new List<string>();
-        public List<int> listbalances = new List<int>();
+        InitializeComponent();
+    }
+    bool blackjack = false;
+    bool poker = false;
+    private void Blackjack_Click(object sender, EventArgs e)
+    {
+        poker = false;
+        blackjack = true;
+    }
+    public List<string> listnames = new List<string>();
+    public List<int> listbalances = new List<int>();
 
-        private void addplayer_Click(object sender, EventArgs e)
+    private void addplayer_Click(object sender, EventArgs e)
+    {
+        if (Name.Text == "" || Balance.Text == "")
         {
-            if (Name.Text == "" || Balance.Text == "")
-            {
-                MessageBox.Show("Please enter a name and balance");
-                return;
-            }
-            if (!int.TryParse(Balance.Text, out int n) || Convert.ToInt32(Balance.Text) < 0)
-            {
-                MessageBox.Show("Please enter a valid balance");
-                return;
-            }
-            if(listnames.Contains(Name.Text))
-            {
-                MessageBox.Show("Player already exists");
-                return;
-            }
-            if(listnames.Count >= 7)
-            {
-                MessageBox.Show("Maximum number of players reached");
-                return;
-            }
-            listnames.Add(Name.Text);
-            listbalances.Add(Convert.ToInt32(Balance.Text));
-            Name.Clear();
-            Balance.Clear();
+            MessageBox.Show("Please enter a name and balance");
+            return;
         }
+        if (!int.TryParse(Balance.Text, out int n) || Convert.ToInt32(Balance.Text) < 0)
+        {
+            MessageBox.Show("Please enter a valid balance");
+            return;
+        }
+        if(listnames.Contains(Name.Text))
+        {
+            MessageBox.Show("Player already exists");
+            return;
+        }
+        if(listnames.Count >= 7)
+        {
+            MessageBox.Show("Maximum number of players reached");
+            return;
+        }
+        listnames.Add(Name.Text);
+        listbalances.Add(Convert.ToInt32(Balance.Text));
+        Name.Clear();
+        Balance.Clear();
+    }
 
-        private void start_Click(object sender, EventArgs e)
-        {
-            Form1 form1 = new Form1(listnames, listbalances);
-            form1.Show();
-            this.Hide();
-        }
+    private void start_Click(object sender, EventArgs e)
+    {
+        Form1 form1 = new Form1(listnames, listbalances);
+        form1.Show();
+        this.Hide();
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,20 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Poker
-{
-    internal static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+namespace Poker;
 
-        }
+internal static class Program
+{
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Home());
+
     }
 }

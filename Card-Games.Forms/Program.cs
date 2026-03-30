8e32c5a468336339e83043eb765d8b4edfcffd55
Derek Gooding <djgooding@live.com>
2026-03-30 08:56:24 -0400

    Project now launches from .Forms project. Dependancies linked

--- Card-Games.Forms/Program.cs
+++ Card-Games.Forms/Program.cs
@@ -1,16 +1,27 @@
-namespace Card_Games.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-static class Program
+namespace Poker
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    internal static class Program
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
-    }    
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Ako pokreneš sa --sim argumentom
+            if (args.Length > 0 && args[0] == "--sim")
+            {
+                mainstart.RunSim(args);  // Pokreni simulaciju
+                return;
+            }
+
+            // Inače pokreni UI
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Home());
+        }
+    }
 }
